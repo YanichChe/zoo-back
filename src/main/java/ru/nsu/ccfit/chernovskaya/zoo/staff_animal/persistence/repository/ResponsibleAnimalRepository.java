8917ad9aa384c5633bcc733d2b91ec7592b91afd
tc <ychernovskaya@gmail.com>
2024-05-16 00:32:47 +0700

    добавила возможность создавать ответственных за животными

--- src/main/java/ru/nsu/ccfit/chernovskaya/zoo/staff_animal/persistence/repository/ResponsibleAnimalRepository.java
+++ src/main/java/ru/nsu/ccfit/chernovskaya/zoo/staff_animal/persistence/repository/ResponsibleAnimalRepository.java
@@ -2,6 +2,7 @@ package ru.nsu.ccfit.chernovskaya.zoo.staff_animal.persistence.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.nsu.ccfit.chernovskaya.zoo.staff_animal.persistence.entity.ResponsibleAnimal;
 import ru.nsu.ccfit.chernovskaya.zoo.staff_animal.persistence.entity.ResponsibleAnimalId;
 import ru.nsu.ccfit.chernovskaya.zoo.util.ApiPathUtils;
@@ -9,4 +10,7 @@ import ru.nsu.ccfit.chernovskaya.zoo.util.ApiPathUtils;
 @RepositoryRestResource(collectionResourceRel = ApiPathUtils.RESPONSE_ANIMAL, path = ApiPathUtils.RESPONSE_ANIMAL)
 public interface ResponsibleAnimalRepository extends PagingAndSortingRepository<ResponsibleAnimal, ResponsibleAnimalId> {
     ResponsibleAnimal findById(ResponsibleAnimalId id);
+
+    @RestResource
+    ResponsibleAnimal save(ResponsibleAnimal accessAnimal);
 }
