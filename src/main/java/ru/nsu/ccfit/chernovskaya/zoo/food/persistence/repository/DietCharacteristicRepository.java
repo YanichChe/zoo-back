fa13ee98068fac1726ad94dc3b6fc9f49cbf0795
tc <ychernovskaya@gmail.com>
2024-05-21 01:06:48 +0700

    add diet characteristic

--- src/main/java/ru/nsu/ccfit/chernovskaya/zoo/food/persistence/repository/DietCharacteristicRepository.java
+++ src/main/java/ru/nsu/ccfit/chernovskaya/zoo/food/persistence/repository/DietCharacteristicRepository.java
@@ -3,10 +3,20 @@ package ru.nsu.ccfit.chernovskaya.zoo.food.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.nsu.ccfit.chernovskaya.zoo.food.persistence.entity.DietCharacteristic;
 import ru.nsu.ccfit.chernovskaya.zoo.util.ApiPathUtils;
 
+import java.util.Optional;
+
 @RepositoryRestResource(collectionResourceRel = ApiPathUtils.DIET_CHARACTERISTICS, path = ApiPathUtils.DIET_CHARACTERISTICS)
 public interface DietCharacteristicRepository extends PagingAndSortingRepository<DietCharacteristic, Integer> {
-    DietCharacteristic findById(Integer id);
+    @RestResource
+    Optional<DietCharacteristic> findById(Integer id);
+
+    @RestResource
+    void delete(DietCharacteristic dietCharacteristic);
+
+    @RestResource
+    DietCharacteristic save(DietCharacteristic dietCharacteristic);
 }
--- src/main/java/ru/nsu/ccfit/chernovskaya/zoo/food/persistence/repository/DietRepository.java
+++ src/main/java/ru/nsu/ccfit/chernovskaya/zoo/food/persistence/repository/DietRepository.java
@@ -1,12 +1,21 @@
 package ru.nsu.ccfit.chernovskaya.zoo.food.persistence.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.nsu.ccfit.chernovskaya.zoo.food.persistence.entity.Diet;
 import ru.nsu.ccfit.chernovskaya.zoo.util.ApiPathUtils;
 
+import java.util.Optional;
+
 @RepositoryRestResource(collectionResourceRel = ApiPathUtils.DIET, path = ApiPathUtils.DIET)
 public interface DietRepository extends PagingAndSortingRepository<Diet, Integer> {
-    Diet findById(Integer id);
+    @RestResource
+    Optional<Diet> findById(Integer id);
+
+    @RestResource
+    void delete(Diet diet);
+
+    @RestResource
+    Diet save(Diet diet);
 }
